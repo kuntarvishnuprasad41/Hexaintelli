7bccaaa91c581fb912c061b86bfb990e05fa9584
Ilan Usman <ilanusman03@gmail.com>
2024-06-28 17:17:20 +0530

    paths

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-import Hero from "@/Components/Hero/Page";
-import { Services } from "@/Components/Services/Page";
+import Hero from "../components/Hero/Page";
+import { Services } from "../components/Services/Page";
 
 export default function Home() {
   const videoSrc = '/hero.mp4';
