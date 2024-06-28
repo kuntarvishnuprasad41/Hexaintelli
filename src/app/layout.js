08c187ebc5e05a0104a257304a3a4eba788cc5e4
Ilan Usman <ilanusman03@gmail.com>
2024-06-28 16:52:41 +0530

    Builderr

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbars from "@/components/Navbar/Page";
-import { NextUIProvider } from "@nextui-org/react";
-import styles from "./RootLayout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,19 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-      </head>
-      <body className={`${inter.className} ${styles.body}`}>
-        <NextUIProvider>
-          <Navbars />
-          <main className={styles.main}>
-            {children}
-          </main>
-        </NextUIProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
-
