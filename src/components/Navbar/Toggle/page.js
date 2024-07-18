0e69555c912eadee6bdcfc8c66b99c7707aeaa13
Ilan Usman <ilanusman03@gmail.com>
2024-07-18 11:18:20 +0530

    nav

--- src/components/Navbar/Toggle/page.js
+++ src/components/Navbar/Toggle/page.js
@@ -1,30 +1,38 @@
 // components/ToggleMenu.js
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { HamburgerMenuIcon, Cross1Icon } from '@radix-ui/react-icons';
 import styles from './page.module.css';
+import { gsap } from 'gsap';
 
 const ToggleMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    if (isOpen) {
+      gsap.to(menuRef.current, { opacity: 1, duration: 0.3, display: 'flex' });
+    } else {
+      gsap.to(menuRef.current, { opacity: 0, duration: 0.3, display: 'none' });
+    }
+  }, [isOpen]);
 
   return (
     <div className={styles.menuContainer}>
       <button onClick={toggleMenu} className={styles.menuButton}>
         {isOpen ? <Cross1Icon /> : <HamburgerMenuIcon />}
       </button>
-      {isOpen && (
-        <nav className={styles.menu}>
-          <a href="/Web">Web Development</a>
-          <a href="/Cyber">Cyber Security</a>
-          <a href="/Machine">Machine Learning</a>
-          <a href="/Cloud">Cloud Computing</a>
-          <a href="/Data">Data Science</a>
-          <a href="/ERP">ERP</a>
-        </nav>
-      )}
+      <nav ref={menuRef} className={styles.menu}>
+        <a href="/Web">Web Development</a>
+        <a href="/Cyber">Cyber Security</a>
+        <a href="/Machine">Machine Learning</a>
+        <a href="/Cloud">Cloud Computing</a>
+        <a href="/Data">Data Science</a>
+        <a href="/ERP">ERP</a>
+      </nav>
     </div>
   );
 };
